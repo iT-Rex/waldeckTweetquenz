da1e5a3fcd401d937c47f2a15593853b08817268
Thomas Schypior <pantrasafe@gmail.com>
2020-07-05 11:25:32 +0200

    initial commit, mostly copypasta from "Twitter API with Python: Part 1 -- Streaming Live Tweets" (https://youtu.be/wlnx-7cm4Gg)

---
+++ tweepy_streamer.py
@@ -0,0 +1,24 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+import twitter_credentials
+
+class StdOutListener(StreamListener):
+
+	def on_data(self, data):
+		print(data)
+		return True
+
+	def on_error(self, status):
+		print(status)
+
+if __name__ == "__main__":
+	listener = StdOutListener()
+	auth = OAuthHandler(twitter_credentials.API_KEY, twitter_credentials.API_SECRET_KEY)
+	auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+	filterStringList = ["nintendo"]
+
+	stream = Stream(auth, listener)
+
+	stream.filter(track = filterStringList)
