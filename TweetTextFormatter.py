f971b935d7eb0e5e57118b906ffdee2e755ebbcd
Thomas Schypior <pantrasafe@gmail.com>
2020-07-06 23:20:55 +0200

    Implement Method chopUpIntoThirtyCharacterLines

--- TweetTextFormatter.py
+++ TweetTextFormatter.py
@@ -1,3 +1,21 @@
 class TweetTextFormatter():
+  def chopUpIntoThirtyCharacterLines(self, tweetText):
+    characterCountLeftToParse = len(tweetText)
+    desiredLineLength = 30
+    nextLineEnd = desiredLineLength
+    nextLineStart = 0
+    choppedUpLines = list()
+    
+    while characterCountLeftToParse > desiredLineLength:
+      choppedUpLines.append(tweetText[nextLineStart:nextLineEnd])
+      characterCountLeftToParse = characterCountLeftToParse - desiredLineLength
+      nextLineStart = nextLineEnd
+      nextLineEnd = nextLineStart + desiredLineLength
+
+    choppedUpLines.append(tweetText[nextLineStart:nextLineStart + characterCountLeftToParse])
+
+    return choppedUpLines
+
   def getFormattedPrintText(self, tweetText):
-    return tweetText
+    formattedPrintText = self.chopUpIntoThirtyCharacterLines(tweetText)
+    return formattedPrintText
