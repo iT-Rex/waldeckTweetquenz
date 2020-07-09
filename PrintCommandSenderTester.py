85cb201a48c660349b28c35ecbe9f475faa8b640
Thomas Schypior <pantrasafe@gmail.com>
2020-07-09 18:57:05 +0200

    add PrintCommandSenderTester.py

---
+++ PrintCommandSenderTester.py
@@ -0,0 +1,6 @@
+import PrintCommandSender
+
+if __name__ == "__main__":
+  printCommandSender = PrintCommandSender.PrintCommandSender()
+  line = "test asdf"
+  printCommandSender.sendPrintLineToConsole(line)
