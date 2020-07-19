364e4659ed3ff042d932cd7663dc8228f4a0f28c
Thomas Schypior <pantrasafe@gmail.com>
2020-07-19 19:04:15 +0200

    use printer file descriptor

--- PrintCommandSender.py
+++ PrintCommandSender.py
@@ -2,5 +2,7 @@ import os
 
 class PrintCommandSender():
   def sendPrintLineToConsole(self, line):
-    printLineCommand = "echo -e '\x1bW1\x1bx1'" + line + "\r > /dev/lp0"
-    os.system(printLineCommand)
+    with open("/dev/lp0", "w") as printer:
+      printer.write("\x1bW1\x1bx1")
+      printer.write(line)
+      printer.write("\r")
