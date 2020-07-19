9b3c9dd3aa3cce4d0011cbaca22ef3bbf8ca78e7
Thomas Schypior <pantrasafe@gmail.com>
2020-07-19 21:19:31 +0200

    fix chop

--- TweetTextFormatter.py
+++ TweetTextFormatter.py
@@ -1,12 +1,9 @@
-from math import ceil
+from textwrap import wrap
 
 class TweetTextFormatter():
   def chop(text, line_length=30):
     for paragraph in text.split("\n"):
-      for line in range(ceil(len(paragraph) / line_length)):
-        start = line * 30
-        end = start + line_length
-        yield paragraph[start:end]
+      yield from wrap(paragraph, width=line_length)
 
   def getFormattedPrintText(self, tweetText):
     formattedPrintText = self.chop(tweetText)
