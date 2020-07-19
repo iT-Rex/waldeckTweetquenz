805fab38f66d95a7e0dc290ee056d0ddb3c17c9b
Elmer de Looff <elmer.delooff@gmail.com>
2020-07-19 21:51:59 +0200

    Reflows tweepy_streamer.py based on PEP8.

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -9,32 +9,44 @@ from printer import Printer
 from utils import reflow
 
 
-class TwitterStreamer():
-  def stream_tweets(self, hash_tag_list):
-    listener = StdOutListener()
-    auth = OAuthHandler(twitter_credentials.API_KEY, twitter_credentials.API_SECRET_KEY)
-    auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+class TwitterStreamer:
+    def stream_tweets(self, hash_tag_list):
+        listener = StdOutListener()
+        auth = OAuthHandler(
+            twitter_credentials.API_KEY, twitter_credentials.API_SECRET_KEY
+        )
+        auth.set_access_token(
+            twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET
+        )
 
-    stream = Stream(auth, listener)
-    stream.filter(track = hash_tag_list)
+        stream = Stream(auth, listener)
+        stream.filter(track=hash_tag_list)
+
 
 class StdOutListener(StreamListener):
-  def on_data(self, data):
-    jsonData = json.loads(data)
-    tweetText = jsonData['text']
+    def on_data(self, data):
+        jsonData = json.loads(data)
+        tweetText = jsonData["text"]
 
-    printer = Printer()
-    for line in reflow(tweetText):
-      printer.print(line)
-    printer.separator()
+        printer = Printer()
+        for line in reflow(tweetText):
+            printer.print(line)
+        printer.separator()
 
-    return True
+        return True
 
-  def on_error(self, status):
-    print(status)
+    def on_error(self, status):
+        print(status)
+
 
 if __name__ == "__main__":
-  hash_tag_list = ['@BurgWaldeck','#waldeckfreakquenz','@WFreakquenz','#digitalfreakquenz','freakquenz']
+    hash_tag_list = [
+        "@BurgWaldeck",
+        "#waldeckfreakquenz",
+        "@WFreakquenz",
+        "#digitalfreakquenz",
+        "freakquenz",
+    ]
 
-  twitterStreamer = TwitterStreamer()
-  twitterStreamer.stream_tweets(hash_tag_list)
+    twitterStreamer = TwitterStreamer()
+    twitterStreamer.stream_tweets(hash_tag_list)
