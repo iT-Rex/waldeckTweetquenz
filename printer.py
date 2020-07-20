1cbad2b8b9e7491707c6e375a35900b7877711a4
Elmer de Looff <elmer.delooff@gmail.com>
2020-07-20 22:15:52 +0200

    Adds 'high_quality' parameter to Printer thad sets double_strike and emphasized printer modes.

--- printer.py
+++ printer.py
@@ -1,43 +1,39 @@
-from itertools import repeat
-
 from utils import character_encoder
 
 
 class Printer:
-    WIDE_MODE = b"\x1bW%d"
-    QUALITY_MODE = b"\x1bx%d"
+    WIDE_MODE = "\x1bW%d"
+    QUALITY_MODE = "\x1bx%d"
+    EMPHASIZED_MODE = "\x1bE"
+    DOUBLE_STRIKE_MODE = "\x1bG"
 
     def __init__(
         self,
         device: str = "/dev/lp0",
         width: int = 1,
-        quality: int = 1,
+        high_quality: bool = True,
         encoding: str = "cp850",
     ):
         self.device = device
         self.encoding = encoding
+        if high_quality:
+            self._write(self.QUALITY_MODE % 1)
+            self._write(self.EMPHASIZED_MODE)
+            self._write(self.DOUBLE_STRIKE_MODE)
         self._write(self.WIDE_MODE % width)
-        self._write(self.QUALITY_MODE % quality)
 
-    def print(self, text: str, weight: int = 2) -> None:
-        """Takes an input string and prints it, terminating with a newline.
-
-        This also implements a printing `weight` that's achieved using a simple
-        carriage return and reprint. For this to work, the given line must not
-        contain its own newlines.
-        """
-        for data in repeat(self._encoder(text + "\r"), weight):
-            self._write(data)
-        self._write(b"\n")
+    def print(self, text: str) -> None:
+        """Takes an input string and prints it, terminating with a newline."""
+        self._write(text + "\n")
 
     def separator(self) -> None:
         """Generates a big blank separator between printed blocks."""
-        self._write(b"\n" * 6)
+        self._write("\n" * 6)
 
-    def _encoder(self, text: str) -> bytes:
+    def _encode(self, text: str) -> bytes:
         encoder = character_encoder(self.encoding)
         return b"".join(map(encoder, text))
 
-    def _write(self, raw: bytes) -> None:
+    def _write(self, text: str) -> None:
         with open(self.device, "wb") as fp:
-            fp.write(raw)
+            fp.write(self._encode(text))
