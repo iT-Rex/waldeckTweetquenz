810ac63e27c1e2d0381c3f3795f883e67871ef24
Tjeerd Visser <tjeerd@awesomeretro.org>
2020-07-26 02:27:08 +0200

    no output, so another approach

--- tweetquenz.py
+++ tweetquenz.py
@@ -66,7 +66,7 @@ class Listener(StreamListener):
 
     def store_raw_status(self, status: Status) -> None:
         if self.rawfile is not None:
-            json.dump(asdict(Status), self.rawfile)
+            json.dump(Status._json, self.rawfile)
             self.rawfile.write("\n")
 
     def store_tweet(self, tweet: Tweet) -> None:
